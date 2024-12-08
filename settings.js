06817e8ee958445af9117bc8b1e879626cb43737
nabhansyafiq <110009070+nabhansyafiq@users.noreply.github.com>
2024-12-09 02:03:11 +0700

    new

---
+++ settings.js
@@ -0,0 +1,117 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗫𝗟𝗜𝗖𝗢𝗡-𝗩𝟰-𝗠𝗗  𝐁𝐎𝐓                                                //
+//                                                                                                      //
+//                                         Ｖ：4.0                                                       //
+//                                                                                                      //
+//                                                                                                      //      
+//               ██╗  ██╗██╗     ██╗ ██████╗ ██████╗ ███╗   ██╗      ██╗   ██╗██╗  ██╗                  //              
+//                ██╗██╔╝██║     ██║██╔════╝██╔═══██╗████╗  ██║      ██║   ██║██║  ██║                  //
+//                ╚███╔╝ ██║     ██║██║     ██║   ██║██╔██╗ ██║█████╗██║   ██║███████║                  // 
+//                ██╔██╗ ██║     ██║██║     ██║   ██║██║╚██╗██║╚════╝╚██╗ ██╔╝╚════██║                  // 
+//               ██╔╝ ██╗███████╗██║╚██████╗╚██████╔╝██║ ╚████║       ╚████╔╝      ██║                  //
+//                ═╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝        ╚═══╝       ╚═╝                  // 
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : XLICON-V4-MD
+//  * @author : salmanytofficial
+//  * @youtube : https://www.youtube.com/@s4salmanyt
+//  * @description : XLICON-V4 ,A Multi-functional whatsapp user bot.
+//*
+//*
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//Instagram: ahmmikun
+//Telegram: t.me/ahmmitech
+//GitHub: @salmanytofficial
+//WhatsApp: +923184070915
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//   * Created By Github: DGXeon.
+//   * Credit To Xeon
+//   * © 2024 XLICON-V3-MD.
+// ⛥┌┤
+// */
+
+const fs = require('fs');
+const chalk = require('chalk');
+
+//owmner v card
+global.ytname = "YT:-" //ur yt chanel name
+global.socialm = "GitHub: -" //ur github or insta name
+global.location = "Indonesia, Semarang, Jawa Tengah" //ur location
+
+//new
+global.botname = ' ©elisal твой кошмар ⛩️' //ur bot name
+global.ownernumber = ['6283852406727', '628895154319']; // Masukkan nomor tambahan di sini
+ //ur owner number, dont add more than one
+global.ownername = '©C.U.W ⛩️' //ur owner name
+global.websitex = "https://chat.whatsapp.com/Kh1kOsK33GCEohpf7ezrVs"
+global.wagc = "https://chat.whatsapp.com/Kh1kOsK33GCEohpf7ezrVs"
+global.themeemoji = '⛩'
+global.wm = "©elisal твой кошмар ⛩️"
+global.botscript = 'https://wa.me/628895154319' //script link
+global.packname = " ©elisal твой кошмар ⛩️"
+global.author = " ©elisal твой кошмар ⛩️"
+global.creator = ["6283852406727@s.whatsapp.net", "628895154319@s.whatsapp.net"];
+global.xprefix = '.'
+global.premium = ['6283852406727', '628895154319']; // Premium Users
+
+
+//bot sett
+global.typemenu = 'v2' // menu type 'v1' => 'v12'
+global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.autoblocknumber = '212' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.antidelete = false //set anti delete 
+global.autoresponseActive = true; // Atur ke false jika tidak ingin autoresponse aktif
+
+
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json'
+
+
+
+global.limit = {
+	free: 100,
+	premium: 999,
+	vip: 'VIP'
+}
+
+global.uang = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	error: 'Error!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	done: 'Done'
+}
+
+global.bot = {
+	limit: 0,
+	uang: 0
+}
+
+global.game = {
+	suit: {},
+	menfes: {},
+	tictactoe: {},
+	kuismath: {},
+	tebakbom: {},
+}
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Updated ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
